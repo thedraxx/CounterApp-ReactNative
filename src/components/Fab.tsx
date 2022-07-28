ec950a5de80373597f995c6d2acada5aa24b19d3
Frank <55867157+thedraxx@users.noreply.github.com>
2022-07-28 13:50:22 -0300

    finish

---
+++ src/components/Fab.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Text, TouchableOpacity, View, StyleSheet} from 'react-native';
+
+// Definimos los props que va a recibir el componente
+interface Props {
+  title: string;
+  position?: 'br' | 'bl';
+  onPress: () => void;
+}
+
+export const Fab = ({title, onPress, position = 'br'}: Props) => {
+  return (
+    <>
+      <TouchableOpacity
+        onPress={onPress}
+        style={position === 'br' ? styles.fabLocationBR : styles.fabLocationBL}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>{title}</Text>
+        </View>
+      </TouchableOpacity>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  fabLocationBR: {
+    position: 'absolute',
+    bottom: 15,
+    right: 0,
+  },
+
+  fabLocationBL: {
+    position: 'absolute',
+    bottom: 15,
+    left: 0,
+  },
+
+  fab: {
+    backgroundColor: '#9151ff',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center',
+  },
+  fabText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+});
--- src/screens/Counter.tsx
+++ src/screens/Counter.tsx
@@ -1,25 +1,36 @@
 import React, {useState} from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import {Fab} from '../components/Fab';
 
 export const Counter = () => {
   const [counter, setCounter] = useState(10);
 
-  const Press = () => {
-    setCounter(counter + 1);
-  };
-
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
-      <Text
-        style={{
-          textAlign: 'center',
-          fontSize: 30,
-          top: -10,
-        }}>
-        Counter:
-        {counter}
-      </Text>
-      <Button title="Click" onPress={Press} />
+    <View style={styles.container}>
+      <Text style={styles.title}>Counter:{counter}</Text>
+      {/* Boton inferior Derecho */}
+      <Fab title="+1" position="br" onPress={() => setCounter(counter + 1)} />
+
+      {/* Boton inferior izquierdo */}
+      <Fab title="-1" position="bl" onPress={() => setCounter(counter - 1)} />
+
+      {/* <TouchableOpacity onPress={PressMinus} style={styles.fabLocationBL}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-1</Text>
+        </View>
+      </TouchableOpacity> */}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    textAlign: 'center',
+    top: -15,
+  },
+});
